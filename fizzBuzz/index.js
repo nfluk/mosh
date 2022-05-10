79ab104b086d9b212539ef51f8317c32a754151d
nfluk <nickylukic@gmail.com>
2022-05-10 21:24:13 +0200

    first commit

---
+++ fizzBuzz/index.js
@@ -0,0 +1,19 @@
+// fizzBuzz in js
+
+function fizzBuzz(n) {
+  if (typeof n !== 'number') {
+    console.log('That is not a number. Please enter a number.');
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
