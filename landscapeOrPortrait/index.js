e9d94a5981994c1c9646dd1053189ef62caac4d2
nfluk <nickylukic@gmail.com>
2022-05-10 21:08:33 +0200

    first commit

---
+++ landscapeOrPortrait/index.js
@@ -0,0 +1,5 @@
+// Take 2 parameters (width & height) and returns if a image is landscape (wider than tall) or portrait (taller than wide)
+
+function isLandscape(width, height) {
+  return width > height;
+}
