07abfe3c805fdcc20b3d2256f686e7c18d647968
nfluk <nickylukic@gmail.com>
2022-06-03 22:17:54 +0200

    added api/courses endpoint with numb array

--- express-demo/index.js
+++ express-demo/index.js
@@ -8,4 +8,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/api/courses', (req, res) => {
+  res.send([1, 2, 3]);
+});
+
 app.listen(port, () => console.log(`Listening on port ${port} ...`));
