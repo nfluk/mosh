4991730e34773b4244cc43285b031c76114ab951
nfluk <nickylukic@gmail.com>
2022-06-05 17:13:23 +0200

    in post: pushed new course and sent it

--- express-demo/index.js
+++ express-demo/index.js
@@ -29,7 +29,10 @@ app.get('/api/courses/:id', (req, res) => {
 app.post('/api/courses', (req, res) => {
   const course = {
     id: courses.length + 1,
+    name: req.body.name,
   };
+  courses.push(course);
+  res.send(course);
 });
 
 // PORT
